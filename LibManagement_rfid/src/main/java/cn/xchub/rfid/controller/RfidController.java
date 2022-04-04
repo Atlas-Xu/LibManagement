7af6e8c835af9f5b9dfa0c36d53d431a678b84e3
techial <dev@techial.cc>
2022-04-04 20:14:01 +0800

    update book query

--- LibManagement_rfid/src/main/java/cn/xchub/rfid/controller/RfidController.java
+++ LibManagement_rfid/src/main/java/cn/xchub/rfid/controller/RfidController.java
@@ -1,11 +1,10 @@
 package cn.xchub.rfid.controller;
 
-import cn.xchub.rfid.domain.RfidRequest;
+import cn.xchub.rfid.domain.GetBookResponse;
 import cn.xchub.rfid.service.LibManagementService;
 import cn.xchub.utils.ResultUtils;
 import cn.xchub.utils.ResultVo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,27 +22,14 @@ public class RfidController {
         this.libManagementService = libManagementService;
     }
 
-    @PostMapping
-    public ResultVo<Object> rfidQuery(@RequestBody RfidRequest request) {
-        final List<String> rfids = request.getRfids()
-                .stream()
-                .filter(Objects::nonNull)
-                .filter(it -> !it.isEmpty())
-                .collect(toList());
+    @GetMapping
+    public ResultVo<Object> getBook() {
+        final List<String> rfidResult = libManagementService.queryByRfid();
+        final List<String> bookCodes = libManagementService.rfidToResultToBookCode(rfidResult);
+        final List<GetBookResponse> books = bookCodes.stream().map(libManagementService::getBook)
+                                                     .filter(Objects::nonNull)
+                                                     .collect(toList());
 
-        final List<String> bookCodes = rfids.stream()
-                .map(libManagementService::rfidToCode)
-                .filter(Objects::nonNull)
-                .filter(it -> !it.isEmpty())
-                .collect(toList());
-
-        return ResultUtils.success(
-                "查询成功",
-                bookCodes.stream()
-                        .map(libManagementService::getBook)
-                        .filter(Objects::nonNull)
-                        .collect(toList())
-        );
-
+        return ResultUtils.success("查询成功", books);
     }
 }
--- LibManagement_rfid/src/main/java/cn/xchub/rfid/service/LibManagementService.java
+++ LibManagement_rfid/src/main/java/cn/xchub/rfid/service/LibManagementService.java
@@ -7,13 +7,16 @@ import cn.xchub.rfid.domain.GetBookResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
-import java.time.Instant;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @Slf4j
@@ -21,16 +24,12 @@ public class LibManagementService {
     private final RestTemplate rest;
     private final LibManagementProperties libManagementProperties;
 
-    private int fCmdRet = 0x30;
-    UHFReader uhf = new UHFReader();
-
     public LibManagementService(LibManagementProperties libManagementProperties) {
         this.rest = new RestTemplateBuilder()
                 .rootUri(libManagementProperties.getApplicationUrl())
                 .build();
         this.libManagementProperties = libManagementProperties;
     }
-
 
     private GetBookResponse fetch(GetBookRequest request) {
         return rest.postForObject(
@@ -40,76 +39,44 @@ public class LibManagementService {
         );
     }
 
-
     public GetBookResponse getBook(String bookCode) {
         return fetch(new GetBookRequest(bookCode));
     }
 
-
-    private static final List<String> hackBookCode = Arrays.asList(
-            "974651231",
-            "4651313213",
-            "646313132",
-            "7846113213",
-            "71313456466",
-            "7841354646",
-            "789461312321",
-            "9746512345"
-    );
+    final UHFReader uhf = new UHFReader();
 
     /**
-     *
      * 开关读卡器端口用的
-     * */
-
+     */
     @PostConstruct
     @Async
     public void OpenCom() {
         byte ComAddr = (byte) 255;
         byte baudRate = (byte) 5;
-        fCmdRet = uhf.AutoOpenCom(ComAddr, baudRate);
+        int fCmdRet = uhf.AutoOpenCom(ComAddr, baudRate);
         if (fCmdRet == 0) {
             log.info("打开串口成功");
-            query();
-            log.info(query().get(0));
+            queryByRfid();
+            log.info(queryByRfid().get(0));
         } else {
             log.info("打开串口失败");
         }
     }
 
-    /**
-     * 读RFID数据块的函数。读出的是EPC
-     * 多张放一起就报错error create beans，是不是加了硬件的参数，所以注入有问题(此处随便写在service里面测试的，如果需要改就把这个和上面那个硬件的函数改在别的地方就可)
-     *
-     */
-    // TODO 该函数每次请求接口时调用
-    @Async
-    @Scheduled(cron = "0/1 * * * * *")
-    public List<String> query(){
-        Set<String> set = new LinkedHashSet<>();
-        List<String> list = new ArrayList<>();
-        String EPC[] = uhf.Inventory();
-        if (EPC == null) return null;
+    public List<String> queryByRfid() {
+        String[] epc = uhf.Inventory();
+        if (epc == null) return null;
 
-        for (String val : EPC) {
-            list.add(val);
-        }
-        set.addAll(list);
-        list.clear();
-        list.addAll(set);
-        // 此处读取出的即为所有书本的ISBN号，每个EPC为16位，ISBN为13为，多3位
-        return list;
+        List<String> result = new ArrayList<>(Arrays.asList(epc));
+        return result.stream().distinct().collect(toList());
     }
 
-    /**
-     * TODO RFID 转 BookCode
-     * EPC数据去掉最后3位即为ISBN
-     */
-    public String rfidToCode(String rfid) {
-        final int index = Math.toIntExact(Instant.now().toEpochMilli() % hackBookCode.size());
-        return hackBookCode.get(index);
+    public List<String> rfidToResultToBookCode(List<String> rfidResult) {
+        return rfidResult.stream()
+                         .filter(Objects::nonNull)
+                         .filter(it -> !it.isEmpty())
+                         .filter(it -> it.length() >= 13)
+                         .map(it -> it.substring(13))
+                         .collect(toList());
     }
-
-
-
 }
